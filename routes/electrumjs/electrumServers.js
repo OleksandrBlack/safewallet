d2a827561d2cf79b3681616a553d72b4cb62b63b
OleksandrBlack <aleksandr.chornuy@gmail.com>
2018-10-25 23:43:52 +0300

    rev

--- routes/electrumjs/electrumServers.js
+++ routes/electrumjs/electrumServers.js
@@ -42,8 +42,6 @@ let electrumServers = {
     serverList: [
       'local.support:40001',
       '45.63.13.60:40001',
-      'electrum.zclassic.community:50002',
-      'zclele.duckdns.org:50002',
     ],
   },
   xsg: {
@@ -55,8 +53,6 @@ let electrumServers = {
     serverList: [
       'local.support:50001',
       '45.63.13.60:50001',
-      'electrumsvr.snowgem.org:50002',
-      'electrumsvr2.snowgem.org:50002',
     ],
   },
 };
