7cd69ac1a06cd5a83b25d91ba15a748dab8e3a38
OleksandrBlack <aleksandr.chornuy@gmail.com>
2018-10-26 12:23:44 +0300

    test

--- binary_mac.sh
+++ binary_mac.sh
@@ -23,17 +23,30 @@ echo =========================================
 echo
 pwd
 echo =========================================
-echo Step: Moving Windows binaries from artifacts to assets/bin/linux64/
-#echo
-mkdir assets/bin
-rm assets/artifacts.ipv6admin.com/latest/linux/iguana
-mv assets/artifacts.ipv6admin.com/latest/linux assets/bin/linux64/
+echo Step: Permission +x for OSX binaries from artifacts to assets/bin/osx/
 echo
+rm assets/artifacts.ipv6admin.com/latest/osx/iguana
+chmod +x assets/artifacts.ipv6admin.com/latest/osx/safecoin*
+
+mkdir assets/bin
+mv assets/artifacts.ipv6admin.com/latest/osx assets/bin/osx
+
+echo Moving legacy libs to assets/bin
+wget https://github.com/Fair-Exchange/bitcore-node-safecoin/releases/download/v0.015/libs_legacy_osx.zip
+checksum=`shasum -a 256 libs_legacy_osx.zip | awk '{ print $1 }'`
+if [ "$checksum" = "e9474aa243694a2d4c87fccc443e4b16a9a5172a24da76af9e5ecddd006649bb" ]; then
+    echo "Checksum is correct."
+    unzip libs_legacy_osx.zip
+    cp -rvf libs_legacy_osx/* assets/bin/osx/.
+  else
+    echo "Checksum is incorrect!"
+    exit 0
+fi
 echo =========================================
 echo Step: Finished Updating binaries from artifacts
 echo
 echo Step: Removing temp-dir binaries
 rm -rvf assets/bin/win64
-rm -rvf assets/bin/osx
+rm -rvf assets/bin/linux64
 rm -rvf assets/artifacts.ipv6admin.com
 echo Step: Finished
